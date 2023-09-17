509506e0a20d679e47044acd9a7d61fdf33c1019
Charan <charankulal0241@gmail.com>
2023-09-17 18:10:03 +0530

    Completed fetching the data from api

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-url =
-  "https://api.currencyapi.com/v3/latest?apikey=cur_live_WpVM9U1FjCDmQwW5nfLyHrWLmiCit5EQWkh0zxBh&currencies=EUR%2CUSD%2CCAD&base_currency=INR";
-
+const populate = async (base, value) => {
+  let myStr = "";
+  url = `https://api.currencyapi.com/v3/latest?apikey=cur_live_WpVM9U1FjCDmQwW5nfLyHrWLmiCit5EQWkh0zxBh&base_currency=${base}`;
+  let response = await fetch(url);
+  let responseJson = await response.json();
+  document.querySelector(".output").style.display="block"
   
+  for (let key of Object.keys(responseJson["data"])) {
+    myStr += `<tr>
+        <td>${key}</td>
+        <td>${responseJson["data"][key]["value"]}</td>
+        <td>${responseJson["data"][key]["value"]*value}</td>
+    </tr>`;
+  }
+  const tableBody = document.querySelector("tbody");
+  tableBody.innerHTML = myStr;
+};
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", () => {
+  
+  var value = parseInt(document.querySelector("input[name='quantity']").value);
+  
+  var base = document.querySelector("select").value;
+  
+
+  populate(base, value);
+});
