8033a6ebdbd7341e7b981362a29e64bc0cb85fc1
Charan <charankulal0241@gmail.com>
2023-09-17 16:32:38 +0530

    Created a form to take input from user

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-console.log("Hello World")
+url =
+  "https://api.currencyapi.com/v3/latest?apikey=cur_live_WpVM9U1FjCDmQwW5nfLyHrWLmiCit5EQWkh0zxBh&currencies=EUR%2CUSD%2CCAD&base_currency=INR";
+
+  
